746b28f1ee18bfd937e8a9b4291099936fb71334
ricardo <ricardo@MBP-de-ricardo.home>
2016-11-14 14:32:22 -0300

    Nuevo commit

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -21,7 +21,7 @@ class ClientController extends Controller
       $client->apepat=$request->apepat;
       $client->apemat=$request->apemat;
       $client->email=$request->email;
-      $client->country_id=$request->country_id; 
+      $client->country_id=$request->country_id;
       $client->save();
 
     }
@@ -37,6 +37,7 @@ class ClientController extends Controller
         $client->apepat=$request->apepat;
         $client->apemat=$request->apemat;
         $client->email=$request->email;
+        $client->country_id=$request->country-_id;
         $client->save();
     }
     public function destroy($id)
